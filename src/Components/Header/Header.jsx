9722f0e74c017a73cc9a4cdbd3965a1dc99fd7a1
unknown <yazdan.jvd.81@gmail.com>
2022-10-09 21:14:45 +0330

    fixed navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import './Header.css';
-import Topbar from '../Topbar/Topbar';
-import Navbar from '../Navbar/Navbar';
+import React, { useEffect, useRef } from "react";
+import "./Header.css";
+import Topbar from "../Topbar/Topbar";
+import Navbar from "../Navbar/Navbar";
 
 export default function Header() {
+  const header__sticky = useRef();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        header__sticky.current.classList.add("header__shrink");
+      } else {
+        header__sticky.current.classList.remove("header__shrink");
+      }
+
+      return () => window.removeEventListener("scroll");
+    });
+  }, []);
+
   return (
-    <div>
-        <Topbar />
-        <Navbar />
+    <div ref={header__sticky}>
+      <Topbar />
+      <Navbar />
     </div>
-  )
+  );
 }
