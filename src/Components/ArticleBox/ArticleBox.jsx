f9f44da19dd15e612fe7f8952d694d1e3a2b389b
unknown <yazdan.jvd.81@gmail.com>
2022-10-09 22:24:19 +0330

    debug component article in home page

--- src/Components/ArticleBox/ArticleBox.jsx
+++ src/Components/ArticleBox/ArticleBox.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './ArticleBox.css';
 import { Link } from 'react-router-dom';
 
-export default function ArticleBox() {
+export default function ArticleBox({img , title , text , article}) {
   return (
     <div className='articlebox__container'>
         <div className="articlebox__wrapper">
-            <img src="./images/blog/1.jpg" alt="" />
-            <h5>نحوه نصب کتابخوانه در پایتون | آموزش نصب کتابخوانه پایتون</h5>
-            <small>زبان پایتون مانند دیگر زبان های برنامه نویسی رایج ,دارای کتابخوانه های مختلفی برای تسریع ...</small>
-            <Link to='/articles/article1'>بیشتر بخوانید</Link>
+            <img src={img} alt="" />
+            <h5>{title}</h5>
+            <small>{text}</small>
+            <Link to={`/articles/article${article}`}>بیشتر بخوانید</Link>
         </div>
     </div>
   )
--- src/Components/Articles/Articles.jsx
+++ src/Components/Articles/Articles.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import './Articles.css';
-import {Link} from 'react-router-dom';
-import {HiArrowSmLeft} from 'react-icons/hi';
-import ArticleBox from '../ArticleBox/ArticleBox';
+import React from "react";
+import "./Articles.css";
+import { Link } from "react-router-dom";
+import { HiArrowSmLeft } from "react-icons/hi";
+import ArticleBox from "../ArticleBox/ArticleBox";
 
 export default function Articles() {
   return (
-    <div className='articler__container'>
-        <div className="articler__wrapper">
-            <div className='articler__wrapper__text'>
-                <div className='articler__wrapper__text__right'>
-                    <h3>جدیدترین مقاله ها</h3>
-                    <p>پیش به سوی ارتقای دانش</p>
-                </div>
-                <div className='articler__wrapper__text__left'>
-                    <Link to='/articles'>تمامی مقاله ها</Link>
-                    <HiArrowSmLeft/>
-                </div>
-            </div>
-            <div className='articler__wrapper__box'>
-                <ArticleBox/>
-                <ArticleBox/>
-                <ArticleBox/>
-            </div>
+    <div className="articler__container">
+      <div className="articler__wrapper">
+        <div className="articler__wrapper__text">
+          <div className="articler__wrapper__text__right">
+            <h3>جدیدترین مقاله ها</h3>
+            <p>پیش به سوی ارتقای دانش</p>
+          </div>
+          <div className="articler__wrapper__text__left">
+            <Link to="/articles">تمامی مقاله ها</Link>
+            <HiArrowSmLeft />
+          </div>
         </div>
+        <div className="articler__wrapper__box">
+          <ArticleBox
+            img="./images/blog/1.jpg"
+            title="نحوه نصب کتابخوانه در پایتون | آموزش نصب کتابخوانه پایتون"
+            text="زبان پایتون مانند دیگر زبان های برنامه نویسی رایج ,دارای کتابخوانه های مختلفی برای تسریع ..."
+            article='1'
+          />
+          <ArticleBox
+            img="./images/blog/3.jpeg"
+            title="بهترین سایت آموزش جاوااسکریپت"
+            text=">زبان پایتون مانند دیگر زبان های برنامه نویسی رایج ,دارای کتابخوانه های مختلفی برای تسریع ..."
+            article='2'
+          />
+          <ArticleBox
+            img="./images/blog/4.png"
+            title="سینک و اسینک"
+            text=">زبان پایتون مانند دیگر زبان های برنامه نویسی رایج ,دارای کتابخوانه های مختلفی برای تسریع ..."
+            article='3'
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
