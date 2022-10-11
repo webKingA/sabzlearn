3fe011e53cfe1d64c43cba5abbe4445f63605e78
unknown <yazdan.jvd.81@gmail.com>
2022-10-11 12:23:36 +0330

    debug category page

---
+++ src/Components/Pagination/Pagination.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Pagination.css';
+
+export default function Pagination() {
+  return (
+    <div className='Pagination__container'>
+        <div className="pagination__wrapper">
+            
+        </div>
+    </div>
+  )
+}
