cd596b6e9bb9a6baa3f13c4f4081c98f772b5acc
unknown <yazdan.jvd.81@gmail.com>
2022-10-12 16:48:07 +0330

    end course page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { useRoutes } from "react-router-dom";
 import routes from "./Routes";
 import Context from "./Context";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
 
--- src/Pages/CourseInfo/CourseInfo.jsx
+++ src/Pages/CourseInfo/CourseInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./CourseInfo.css";
 import Header from "../../Components/Header/Header";
 import CoursePage from "../../Components/CoursePage/CoursePage";
+import Footer from '../../Components/Footer/Footer';
 
 export default function CourseInfo() {
   return (
@@ -9,56 +10,7 @@ export default function CourseInfo() {
       <div className="courseinfo__wrapper">
         <Header />
         <CoursePage />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <Footer />
       </div>
     </div>
   );
